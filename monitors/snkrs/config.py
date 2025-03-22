7766eca763e4193f3a8bbbcf88e6ad33fb586586
Vojtaam <blazusvojta@gmail.com>
2025-03-22 16:40:58 +0100

    config.py

--- monitors/snkrs/config.py
+++ monitors/snkrs/config.py
@@ -1,18 +1,18 @@
 # --------------------- WEBHOOK URL ---------------------
-WEBHOOK = ""
+WEBHOOK = "https://discordapp.com/api/webhooks/1352760322762604596/o8O8skYeS1uNGHy-PXatjERYy4mYhxHRf3yaxPoKV9Dgp7sj94cC-m_5b1sCmccHDknC"
 
 # --------------------- SNKRS DETAILS ---------------------
-LOCATION = ""
-LANGUAGE = ""
+LOCATION = "CZ"
+LANGUAGE = "cs"
 
 # --------------------- FREE PROXY ---------------------
 # A single or multiple locations can be added in the array (e.g. ["GB"] or ["GB", "US"])
-ENABLE_FREE_PROXY = ""
-FREE_PROXY_LOCATION = ""
+ENABLE_FREE_PROXY = False
+FREE_PROXY_LOCATION = ["GB"]
 
 # --------------------- DELAY ---------------------
 # Delay between site requests
-DELAY = ""
+DELAY = 5
 
 # --------------------- OPTIONAL PROXY ---------------------
 # Proxies must follow this format: "<proxy>:<port>" OR "<proxy_username>:<proxy_password>@<proxy_domain>:<port>")
@@ -25,6 +25,6 @@ PROXY = []
 KEYWORDS = []
 
 # --------------------- DISCORD BOT FEATURES ---------------------
-USERNAME = ""
+USERNAME = "Nike SNKRS"
 AVATAR_URL = ""
-COLOUR = ""
+COLOUR = "16777215"
